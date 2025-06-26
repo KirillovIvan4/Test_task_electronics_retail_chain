e5e1b3fac62c88c458de141369792ae242e43655
Ivan Kirllov <kirillovio@mail.ru>
2025-06-26 21:56:10 +0300

    настроил админку

--- networks/admin.py
+++ networks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from networks.models import NetworkNode, Product
+
+
+@admin.register(NetworkNode)
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'node_type')
+    list_filter = ('country', 'city')
+    search_fields = ('country', 'city')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'model')
+    list_filter = ('name', 'model')
+    search_fields = ('name', 'model')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'is_staff', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email')
